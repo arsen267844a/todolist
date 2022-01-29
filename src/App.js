ba2c7515529e4981dbb09c15634f7582093c0a7c
arsen-work <arsen172317@gmail.com>
2022-01-29 20:19:57 +0400

    clear code

--- src/App.js
+++ src/App.js
@@ -1,32 +1,8 @@
-import { ComponentOne } from "./components/ComponentOne";
-import { ComponentTwo } from "./components/ComponentTwo";
-import {  useState } from "react";
-
-const initialState = {
-  isShow: true,
-  age: 45,
-};
-
 export const App = () => {
-  const [state, setState] = useState(initialState);
-  const { isShow, age } = state;
-
-  const func = (value) => {
-    if (value === "one") {
-      setState((prevState) => ({ ...prevState, isShow: true }));
-    } else if (value === "two") {
-      setState((prevState) => ({ ...prevState, isShow: false }));
-    }
-  };
-  
-  
-
+ 
   return (
     <div>
-      {isShow ? <ComponentOne /> : <ComponentTwo />}
-
-      <button onClick={() => func("one")}>one</button>
-      <button onClick={() => func("two")}>Two</button>
+     <h1>react</h1>
     </div>
   );
 };
